f108a40b2e9153f5b0a2362acf3cccc8708c6c71
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-02-03 21:10:36 +0100

    update template and order only dependency of run and lldb targets

--- plugin-template/source/main.cpp
+++ plugin-template/source/main.cpp
@@ -1,31 +1,23 @@
-// Copyright (C) <Year>  <Name>
-// All rights reserved.
+/// Copyright (C) <Year> <Name>
+/// All rights reserved.
 
 #include <c4d.h>
 #include <res/c4d_symbols.h>
 
-// ===========================================================================
-// ===========================================================================
-Bool PluginStart();
-Bool PluginMessage(Int32, void*);
-void PluginEnd();
 
-// ===========================================================================
-// ===========================================================================
+
 Bool PluginStart()
 {
-  GePrint("Hello, World! from the craftr-c4d Template Project!");
+  GePrint("Hello, World! from the Craftr maxon.c4d Template Project!");
   return true;
 }
 
-// ===========================================================================
-// ===========================================================================
+
 void PluginEnd()
 {
 }
 
-// ===========================================================================
-// ===========================================================================
+
 Bool PluginMessage(Int32 msg, void* pdata)
 {
   switch (msg) {
